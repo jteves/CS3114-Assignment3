19bafe8f9bb12f76d599cc688befb9d8cda37b9a
Jacob Teves <jacobteves@gmail.com>
2016-03-21 01:33:05 -0400

    First Commit and Push to the Master Branch on Git for Assignment 3

---
+++ src/Mergesort.java
@@ -0,0 +1,21 @@
+/**
+ * { your description of the project here }
+ */
+
+/**
+ * The class containing the main method, the entry point of the application.
+ * 
+ * @author { your name here }
+ * @version { put something here }
+ */
+public class Mergesort {
+
+	/**
+	 * The entry point of the application
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
+}
---
+++ src/MergesortTest.java
@@ -0,0 +1,24 @@
+import student.TestCase;
+
+/**
+ * @author CS3114 staff
+ * @version 1
+ */
+
+public class MergesortTest 
+	extends TestCase {
+	
+	/**
+	 * This method sets up the tests that follow.
+	 */
+	public void setUp() {
+		// no op
+	}
+	
+	public void testInit() {
+		Mergesort merge = new Mergesort();
+		assertNotNull(merge);
+		Mergesort.main(null);
+		assertFuzzyEquals("Hello, World", systemOut().getHistory());
+	}
+}
