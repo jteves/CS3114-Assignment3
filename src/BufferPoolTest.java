3ed437aac0c7d60ff974387eda1f97258def3888
Andrew Williams <dwill225@vt.edu>
2016-04-04 21:18:15 -0400

    File /C:/Users/Drew/git/CS3114-Assignment_3/src/BufferPoolTest.java changed at Mon Apr 04 21:18:15 EDT 2016

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -11,31 +11,31 @@ public class BufferPoolTest extends TestCase {
      */
     public void setUp() {
         FileGenerator whoCares = new FileGenerator();
-        String[] list = {"-a", "DrewTest.txt", "1000"};
+        String[] list = {"-a", "DrewTest.txt", "1"};
         try {
             whoCares.generateFile(list);
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        pool = new BufferPool("DrewTest.txt", 5);
-//        sort = new Mergesort(1000 * 4096, pool);
-//        pool.addMerge(sort);
+        pool = new BufferPool("DrewTest.txt", 2);
+        sort = new Mergesort(1 * 4096, pool);
+        pool.addMerge(sort);
     }
     
     public void test() {
-//        sort.sort();
-//        sort.getPool().flush();
-//        
-//        System.out.println(sort.numRead);
-//        System.out.println(sort.numWrite);
-//        System.out.println(sort.numCache);
-//        
-//        // Test that the file is properly sorted
-//        try {
-//			assertTrue((new CheckFile()).checkFile("DrewTest.txt"));
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+        sort.sort();
+        sort.getPool().flush();
+        
+        System.out.println(sort.numRead);
+        System.out.println(sort.numWrite);
+        System.out.println(sort.numCache);
+        
+        // Test that the file is properly sorted
+        try {
+			assertTrue((new CheckFile()).checkFile("DrewTest.txt"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
     
 //    public void test2() {
