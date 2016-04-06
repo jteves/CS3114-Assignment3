eb124f1449d966ee58684c9e714f8c969c6eb6bd
Jacob Teves <jacobteves@gmail.com>
2016-04-06 00:34:44 -0400

    File /C:/Users/jacobteves/workspace/eclipse/Assignment3/src/MergesortTest.java changed at Wed Apr 06 00:34:44 EDT 2016

--- src/MergesortTest.java
+++ src/MergesortTest.java
@@ -26,11 +26,15 @@ public class MergesortTest
 		sort.upRead();
 		sort.upWrite();
 		sort.getPool();
-		
+
 		assertEquals(8, pool.sendToMerge(0, 8).length);
-		pool.sendToMerge(0, 8);
-		pool.sendToMerge(8, 16);
-		pool.sendToMerge(8, 16);
+		assertEquals(16, pool.sendToMerge(0, 16).length);
+		assertEquals(24, pool.sendToMerge(0, 24).length);
+		assertEquals(32, pool.sendToMerge(0, 32).length);
+		assertEquals(8, pool.sendToMerge(8, 16).length);
+		assertEquals(8, pool.sendToMerge(16, 24).length);
+		assertEquals(8, pool.sendToMerge(24, 32).length);
+		assertEquals(4096, pool.sendToMerge(0, 4096).length);
 		byte[] hello = {1,2, 3, 4};
 		pool.recieveFromMerge(0, hello);
 		pool.remove();
